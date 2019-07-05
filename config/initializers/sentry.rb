37f8019dbf022f3bdb47cd42d26df2ff09785f55
Jonathan VUKOVICH TRIBOUHARET <jonathan@eivo.fr>
2019-07-05 15:56:18 +0200

    Sentry - catch more exceptions

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -5,4 +5,10 @@ require 'raven'
 Raven.configure do |config|
   config.environments = %w[ staging production ]
   config.dsn = Rails.application.credentials.dig(:sentry, :dsn)
+  config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
+
+  config.excluded_exceptions = [
+    'ActionController::RoutingError',
+    'ActionController::InvalidAuthenticityToken'
+  ]
 end
