e4480e7d38b3665f1eb1e6645191c7d14f4b70a5
Jonathan VUKOVICH-TRIBOUHARET <jonathan@eivo.fr>
2021-08-05 17:10:47 +0200

    Add pagination infinite options

--- app/controllers/eivo/concerns/pagination.rb
+++ app/controllers/eivo/concerns/pagination.rb
@@ -5,7 +5,7 @@ module EIVO
     module Pagination
       extend ::ActiveSupport::Concern
 
-      def paginate(collection)
+      def paginate(collection, options = {})
         unless ::ActiveModel::Type::Boolean.new.cast(params[:pagination]) == false
           limit = 50
           if params[:limit]
@@ -22,22 +22,32 @@ module EIVO
       def pagination_options(collection)
         options = {
           is_collection: true,
-          meta: {
-            total: collection.total_count,
-            pages: collection.total_pages
-          },
           links: {
             self: url_for(params.to_unsafe_h.merge(page: collection.current_page)),
             first: url_for(params.to_unsafe_h.merge(page: 1)),
-            last: url_for(params.to_unsafe_h.merge(page: collection.total_pages))
           }
         }
 
-        if !collection.out_of_range? && !collection.first_page?
-          options[:links][:prev] = url_for(params.to_unsafe_h.merge(page: collection.prev_page))
-        end
-        if !collection.out_of_range? && !collection.last_page?
-          options[:links][:next] = url_for(params.to_unsafe_h.merge(page: collection.next_page))
+        # avoid count request on large table
+        if params[:pagination] == 'infinite'
+          options[:links][:next] = url_for(params.to_unsafe_h.merge(page: collection.current_page + 1))
+
+          if !collection.first_page?
+            options[:links][:prev] = url_for(params.to_unsafe_h.merge(page: collection.current_page - 1))
+          end
+        else
+          options[:links][:last] = url_for(params.to_unsafe_h.merge(page: collection.total_pages))
+          options[:meta] = {
+            total: collection.total_count,
+            pages: collection.total_pages
+          }
+
+          if !collection.out_of_range? && !collection.last_page?
+            options[:links][:next] = url_for(params.to_unsafe_h.merge(page: collection.next_page))
+          end
+          if !collection.out_of_range? && !collection.first_page?
+            options[:links][:prev] = url_for(params.to_unsafe_h.merge(page: collection.prev_page))
+          end
         end
 
         options
